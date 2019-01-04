1a915dc9b6560c9d7cea77dd9a48ec13745b0103
rafael-mq <rafael.mqueiroz@outlook.com>
2019-01-04 16:56:32 -0300

    Add tests to get todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,11 +1,20 @@
+/* eslint-disable no-undef */
 const expect = require('expect')
 const request = require('supertest')
 
 const { app } = require('./../server')
 const { Todo } = require('./../models/todo')
 
+const dummyTodos = [{
+  text: 'Fisrt dummy todo'
+}, {
+  text: 'Second dummy todo'
+}]
+
 beforeEach(done => {
-  Todo.deleteMany({}).then(() => done())
+  Todo.deleteMany({}).then(() => {
+    return Todo.insertMany(dummyTodos)
+  }).then(() => done())
 })
 
 describe('POST /todos', () => {
@@ -24,7 +33,7 @@ describe('POST /todos', () => {
           return done(err)
         }
 
-        Todo.find().then(todos => {
+        Todo.find({ text }).then(todos => {
           expect(todos.length).toBe(1)
           expect(todos[0].text).toBe(text)
           done()
@@ -40,9 +49,21 @@ describe('POST /todos', () => {
       .end((err, res) => {
         if (err) { return done(err) }
         Todo.find().then(todos => {
-          expect(todos.length).toBe(0)
+          expect(todos.length).toBe(2)
           done()
         }).catch(e => done(e))
       })
+  })
+})
+
+describe('GET /todos', () => {
+  it('should get all todos', done => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todos.length).toBe(2)
+      })
+      .end(done)
   })
 })
